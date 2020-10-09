aa75240cfc220049e1bbb65898bf038d913c6aa1
Elodie Le Gal <elodi.le.gal@gmail.com>
2020-10-09 12:14:03 +0200

    javadoc

--- src/aco_tp2_api/CompatibilityChecker.java
+++ src/aco_tp2_api/CompatibilityChecker.java
@@ -4,8 +4,18 @@ import java.util.Set;
 
 public interface CompatibilityChecker {
 
+	/**
+	 * Retourne un ensemble de PartType contenant les parties incompatibles avec le parametre reference
+	 * @param reference Le PartType dont on veut obtenir l'ensemble des incompatibilités
+	 * @return un ensemble de PartType incompatibles avec le parametre reference
+	 */
     Set<PartType> getIncompatibilities(PartType reference);
 
+    /**
+	 * Retourne un ensemble de PartType contenant les parties obligatoires pour le parametre reference
+	 * @param reference Le PartType dont on veut obtenir l'ensemble des partie obligatoire
+	 * @return un ensemble de PartType obligatoires pour le parametre reference
+	 */
     Set<PartType> getRequirements(PartType reference);
 
 }
--- src/aco_tp2_api/CompatibilityManager.java
+++ src/aco_tp2_api/CompatibilityManager.java
@@ -4,12 +4,32 @@ import java.util.Set;
 
 public interface CompatibilityManager extends CompatibilityChecker {
 
+	/**
+	 * Ajoute l'incompatibilité entre le PartType reference et l'ensemnle de PartType target
+	 * @param reference le PartType auquel on veut ajouter l'incompatibilité
+	 * @param target l'ensemble de PartType auquel on veut ajouter l'incompatibilité
+	 */
     void addIncompatibilities(PartType reference, Set<PartType> target);
 
+    /**
+     * Supprime l'incompatibilité entre le PartType reference et le PartType target
+     * @param reference le PartType auquel on veut supprimer l'incompatibilité
+     * @param target le 2e  PartType auquel on veut supprimer l'incompatibilité
+     */
     void removeIncompatibility(PartType reference, PartType target);
 
+    /**
+	 * Ajoute l'obligation entre le PartType reference et l'ensemnle de PartType target
+	 * @param reference le PartType auquel on veut ajouter l'obligation
+	 * @param target l'ensemble de PartType auquel on veut ajouter l'obligation
+	 */
     void addRequirements(PartType reference, Set<PartType> target);
 
+    /**
+     * Supprime l'obligation entre le PartType reference et le PartType target
+     * @param reference le PartType auquel on veut supprimer l'obligation
+     * @param target le 2e  PartType auquel on veut supprimer l'obligation
+     */
     void removeRequirement(PartType reference, PartType target);
 
 }
