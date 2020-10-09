0022330a8f0295990365151d82fbf6d75bbfc9b5
Elodie Le Gal <elodi.le.gal@gmail.com>
2020-10-09 12:18:31 +0200

    javadoc

--- src/aco_tp2_api/Configuration.java
+++ src/aco_tp2_api/Configuration.java
@@ -4,10 +4,26 @@ import java.util.Set;
 
 public interface Configuration {
 
+	/**
+	 * Defini si une configuration est valide
+	 * Une configuration est valide si elle respecte les regles d'incompatibilites et d'obligation
+	 * Une configuration n'a pas besoin d'etre complete pour etre valide
+	 * @return true si la configuration est valide
+	 */
     boolean isValid();
 
+    /**
+     * Defini si une configuration est complete
+     * Une configuration est complete si elle contient un element de chaque categories
+     * Une configuration n'a pas besoin d'etre valide pour etre complete
+     * @return true si la configuration est complete
+     */
     boolean isComplete();
 
+    /**
+     * Retourne l'ensemble non-modifiable des PartType selectionnes
+     * @return l'ensemble non-modifiable des PartType selectionnes
+     */
     Set<PartType> getSelectedParts();
 
     void selectPart(PartType chosenPart);
