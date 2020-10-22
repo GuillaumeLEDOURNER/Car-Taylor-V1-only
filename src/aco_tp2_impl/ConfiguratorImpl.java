8573b4118ad71fa4fd713457cafbf79feab433bf
Elodie Le Gal <elodi.le.gal@gmail.com>
2020-10-22 15:17:18 +0200

    fin seance 2

--- src/aco_tp2_impl/ConfiguratorImpl.java
+++ src/aco_tp2_impl/ConfiguratorImpl.java
@@ -1,7 +1,6 @@
 package aco_tp2_impl;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 import aco_tp2_api.Category;
 import aco_tp2_api.CompatibilityChecker;
@@ -11,34 +10,106 @@ import aco_tp2_api.PartType;
 
 public class ConfiguratorImpl implements Configurator {
 	
-	Set <Category> categories;
+	HashMap <Category,Set<PartType>> map;
+	ConfigurationImpl conf;
+	CompatibilityManagerImpl compMan;
 	
 	public ConfiguratorImpl() {
-		this.categories = new HashSet<>();
+		conf = new ConfigurationImpl();
+		compMan = new CompatibilityManagerImpl();
+		
+		initialization();		
 	}
 
 	@Override
-	public Set<Category> getCategories() {
-		Set<Category> copy = categories;
-		return copy;
+	public Set<Category> getCategories(){
+		return map.keySet();
 	}
 
 	@Override
 	public Set<PartType> getVariants(Category category) {
-		// TODO Auto-generated method stub
-		return null;
+		return map.get(category);
 	}
 
 	@Override
 	public Configuration getConfiguration() {
-		// TODO Auto-generated method stub
-		return null;
+		return conf;
 	}
 
 	@Override
 	public CompatibilityChecker getCompatibilityChecker() {
-		// TODO Auto-generated method stub
-		return null;
+		return compMan;
+	}
+	
+	private void initialization() {
+		map = new HashMap<>();
+		
+		//Creation des Category
+		Category engine = new CategoryImpl("Engine");
+		Category trans = new CategoryImpl("trans");
+		Category ext = new CategoryImpl("ext");
+		Category inte = new CategoryImpl("interieur");
+		
+		//Creations des PartType engine
+		PartType eg100 = new PartTypeImpl("eg100", engine);
+		PartType eg133 = new PartTypeImpl("eg133", engine);
+		PartType eg210 = new PartTypeImpl("eg210", engine);
+		PartType ed110 = new PartTypeImpl("ed110", engine);
+		PartType ed180 = new PartTypeImpl("ed180", engine);
+		PartType eh120 = new PartTypeImpl("eh120", engine);
+		
+		//Creations des PartType transmission
+		PartType tm5 = new PartTypeImpl("tm5", trans);
+		PartType tm6 = new PartTypeImpl("tm6", trans);
+		PartType ta5 = new PartTypeImpl("ta5", trans);
+		PartType ts6 = new PartTypeImpl("ts6", trans);
+		PartType tsf7 = new PartTypeImpl("tsf7", trans);
+		PartType tc120 = new PartTypeImpl("tc120", trans);		
+		
+		//Creations des PartType exterieur
+		PartType xc = new PartTypeImpl("xc", ext);
+		PartType xm = new PartTypeImpl("xm", ext);
+		PartType xs = new PartTypeImpl("xs", ext);
+		
+		//Creations des PartType interieur
+		PartType in = new PartTypeImpl("in", inte);
+		PartType ih = new PartTypeImpl("ih", inte);
+		PartType is = new PartTypeImpl("is", inte);
+		
+		//Creation du Set des PartType engine
+		Set<PartType> engines = new HashSet<>();
+		engines.add(eg100);
+		engines.add(eg133);
+		engines.add(eg210);
+		engines.add(ed110);
+		engines.add(ed180);
+		engines.add(eh120);
+		
+		//Creation du Set des PartType trans
+		Set<PartType> transs = new HashSet<>();
+		transs.add(tm5);
+		transs.add(tm6);
+		transs.add(ta5);
+		transs.add(ts6);
+		transs.add(tsf7);
+		transs.add(tc120);
+		
+		//Creation du Set des PartType trans
+		Set<PartType> exts = new HashSet<>();
+		exts.add(xc);
+		exts.add(xm);
+		exts.add(xs);
+		
+		//Creation du Set des PartType trans
+		Set<PartType> ints = new HashSet<>();
+		ints.add(in);
+		ints.add(ih);
+		ints.add(is);
+		
+		map.put(engine,engines);
+		map.put(trans,transs);
+		map.put(ext,exts);
+		map.put(inte,ints);
 	}
 
 }
--- test/TestConfiguration.java
+++ test/TestConfiguration.java
@@ -4,39 +4,39 @@ import org.junit.jupiter.api.Test;
 
 import aco_tp2_impl.*;
 import aco_tp2_api.*;
+import java.util.*;
 
 class TestConfiguration {
 
 	@Test
 	void test01_confVide() {
-		Configuration conf = new ConfigurationImpl();
+		Configurator conf = new ConfiguratorImpl();
 		
-		assertTrue(conf.getSelectedParts().isEmpty());
+		assertTrue(conf.getConfiguration().getSelectedParts().isEmpty());
 	}
 	
 	@Test
-	void test02_confValide() {
-		Configuration conf = new ConfigurationImpl();
-		Category engine = new CategoryImpl("Engine");
-		Category trans = new CategoryImpl("trans");
-		Category ext = new CategoryImpl("ext");
-		Category inte = new CategoryImpl("interieur");
+	void test02_confClear() {
+		Configurator conf = new ConfiguratorImpl();
 		
-		PartType g100 = new PartTypeImpl("g100", engine);
-		PartType tm5 = new PartTypeImpl("tm5", trans);
-		PartType xc = new PartTypeImpl("xc", ext);
-		PartType in = new PartTypeImpl("in", inte);
+		conf.getConfiguration().clear();
 		
-		conf.selectPart(g100);
-		conf.selectPart(tm5);
-		conf.selectPart(xc);
-		conf.selectPart(in);
+		assertTrue(conf.getConfiguration().getSelectedParts().isEmpty());
+	}
+	
+	@Test
+	void test03_confValide() {
+		Configurator conf = new ConfiguratorImpl();
 		
-		for (PartType p : conf.getSelectedParts()) {
+		/*Set<Category> cats = conf.getCategories();
+		Category c = 	
+		Set<PartType> engines = conf.getVariants(c);*/
+		
+		for (PartType p : conf.getConfiguration().getSelectedParts()) {
 			System.out.print(p.getName()+" + ");
 		}
 		
-		assertTrue(conf.isComplete());
+		assertTrue(conf.getConfiguration().isComplete());
 	}
 
 }
