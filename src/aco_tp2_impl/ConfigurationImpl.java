315033d7183093d580d53d0476be790e35f2fb8e
Elodie Le Gal <elodi.le.gal@gmail.com>
2020-10-22 12:23:36 +0200

    implementation de ConfigurationImpl

--- src/aco_tp2_impl/ConfigurationImpl.java
+++ src/aco_tp2_impl/ConfigurationImpl.java
@@ -1,53 +1,81 @@
 package aco_tp2_impl;
 
-import java.util.Set;
+import java.util.*;
 
 import aco_tp2_api.Category;
 import aco_tp2_api.Configuration;
 import aco_tp2_api.PartType;
 
 public class ConfigurationImpl implements Configuration {
+	
+	Set <PartType> conf;
 
 	@Override
 	public boolean isValid() {
-		// TODO Auto-generated method stub
-		return false;
+		Set <PartType> obligatoire;
+		CompatibilityManagerImpl cm = new CompatibilityManagerImpl();
+		for (PartType p : conf) {
+			obligatoire = cm.getRequirements(p);
+			if(!conf.containsAll(obligatoire)) {
+				return false;
+			}
+		}
+		Set <PartType> interdit;
+		for(PartType p : conf) {
+			interdit = cm.getIncompatibilities(p);
+			for (PartType p2 : interdit) {
+				if (conf.contains(p2)){
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 
 	@Override
 	public boolean isComplete() {
-		// TODO Auto-generated method stub
-		return false;
+		ConfiguratorImpl cg = new ConfiguratorImpl();
+		Set<Category> setCat = cg.getCategories();
+		Set<Category> setCatConf = new HashSet<>();
+		for (PartType p : conf) {
+			setCatConf.add(p.getCategory());
+		}
+		return setCatConf.equals(setCat);
 	}
 
 	@Override
 	public Set<PartType> getSelectedParts() {
-		// TODO Auto-generated method stub
-		return null;
+		Set<PartType> copy = conf;
+		return copy;
 	}
 
 	@Override
 	public void selectPart(PartType chosenPart) {
-		// TODO Auto-generated method stub
-
+		conf.add(chosenPart);
 	}
 
 	@Override
 	public PartType getSelectionForCategory(Category category) {
-		// TODO Auto-generated method stub
+		for (PartType p : conf) {
+			if (p.getCategory().getName().equals(category.getName())) {
+				return p;
+			}
+		}
 		return null;
 	}
 
 	@Override
 	public void unselectPartType(Category categoryToClear) {
-		// TODO Auto-generated method stub
-
+		for (PartType p : conf) {
+			if (p.getCategory().getName().equals(categoryToClear.getName())) {
+				conf.remove(p);
+			}
+		}
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-
+		conf.clear();
 	}
 
 }
